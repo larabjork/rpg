800e008236c2c06510e880e6539e0f80a966e7cf
Celeste Morton and Liam Kenna <celeste+liam@>
2019-11-14 11:23:52 -0800

    add test for Game constructor
    Co-Authored-By: Celeste Morton and Liam Kenna <liam@>

--- __test__/rpg.test.js
+++ __test__/rpg.test.js
@@ -1,10 +1,24 @@
 import { House } from './../src/js/house.js';
+import { Game } from './../src/js/game.js';
 
-describe('House',() => {
-  test('should create a house object with name, leader, and army', () =>{
+describe('House', () => {
+
+  test('should create a house object with name, leader, and army', () => {
     const newHouse = new House("Lannister", "Circe");
     expect(newHouse.name).toEqual("Lannister");
     expect(newHouse.leader).toEqual("Circe");
     expect(newHouse.army).toEqual(100);
+  });
+
+});
+
+describe ('Game', () => {
+
+  test('should create a new game with an empty array of houses that is not ready to play and is not over', () => {
+    const newGame = new Game();
+    expect(Array.isArray(newGame.houses)).toEqual(true);
+    expect(newGame.houses.length === 0).toEqual(true);
+    expect(newGame.isReady).toEqual(false);
+    expect(newGame.isOver).toEqual(false);
   });
 });
